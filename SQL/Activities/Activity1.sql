3901a43dd5883c96f12141f93f35e259ac1f91a9
sushma-Gitacc <77955574+sushma-Gitacc@users.noreply.github.com>
2022-02-09 19:12:45 +0530

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,18 @@
+REM   Script: Activity 1
+REM   create table
+
+CREATE Table Customers( 
+CustomerID int, 
+CutomerName varchar(52) 
+);
+
+CREATE TABLE salesman( 
+salesman_id int, 
+salesman_name varchar(20), 
+salesman_city varchar(20), 
+comission int 
+);
+
+DESCRIBE salesman
+
+
